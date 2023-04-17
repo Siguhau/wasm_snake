5874837e33526f274162a7bf4c47e66a69c65844
Siguhau <sigurd.haua@gmail.com>
2023-04-17 21:20:01 +0200

    init cargo setup

---
+++ snake_game/target/debug/deps/snake_game-2047cda3afc9e664.d
@@ -0,0 +1,5 @@
+c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-2047cda3afc9e664.rmeta: src\main.rs
+
+c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-2047cda3afc9e664.d: src\main.rs
+
+src\main.rs:
---
+++ snake_game/target/debug/deps/snake_game-605d5e3429df97d5.d
@@ -0,0 +1,5 @@
+c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-605d5e3429df97d5.rmeta: src\main.rs
+
+c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-605d5e3429df97d5.d: src\main.rs
+
+src\main.rs:
