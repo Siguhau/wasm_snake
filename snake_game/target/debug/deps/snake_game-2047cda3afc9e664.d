255a71861b8429fb46d40edd2f6ae96f97a04ab2
Siguhau <sigurd.haua@gmail.com>
2023-04-17 22:23:58 +0200

    add .gitignore

--- snake_game/target/debug/deps/snake_game-2047cda3afc9e664.d
+++
@@ -1,5 +0,0 @@
-c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-2047cda3afc9e664.rmeta: src\main.rs
-
-c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-2047cda3afc9e664.d: src\main.rs
-
-src\main.rs:
--- snake_game/target/debug/deps/snake_game-605d5e3429df97d5.d
+++
@@ -1,5 +0,0 @@
-c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-605d5e3429df97d5.rmeta: src\main.rs
-
-c:\Users\sigur\Documents\skole\wasm_snake\snake_game\target\debug\deps\snake_game-605d5e3429df97d5.d: src\main.rs
-
-src\main.rs:
